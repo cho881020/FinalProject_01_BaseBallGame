96a51d44990404535447f5bbba1a8eac44f00229
kws970609 <kws970609@users.noreply.github.com>
2020-03-23 20:56:23 +0900

    입력시 스트라이크/볼 갯수 판정

--- app/src/main/java/kr/co/tjoeun/finalproject_01_baseballgame/MainActivity.java
+++ app/src/main/java/kr/co/tjoeun/finalproject_01_baseballgame/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 
 import java.util.ArrayList;
@@ -17,6 +18,8 @@ public class MainActivity extends BaseActivity {
 
     List<Message> messageList = new ArrayList<>();
     MessageAdapter adapter = null;
+//    컴퓨터가 내는 숫자 3자리가 담길 배열
+    int[] questionNumArr = new int[3];
 
 
     ActivityMainBinding binding = null;
@@ -29,6 +32,7 @@ public class MainActivity extends BaseActivity {
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
         setupEvents();
         setValues();
+        makeQuestion();
     }
 
     @Override
@@ -48,7 +52,14 @@ public class MainActivity extends BaseActivity {
 //                입력칸 비우기
                 binding.numInputEdt.setText("");
 
+//              리스트뷰 끌어내림
                 binding.messageListView.smoothScrollToPosition(messageList.size());
+
+//                ?S?B인지 컴퓨터가 대답하게 해주기
+//                입력값을 => int로 바꿔서 => 메쏘드에 전달.
+
+
+                checkStrikeAndBall(Integer.parseInt(inputNumStr));
 
             }
         });
@@ -67,7 +78,96 @@ public class MainActivity extends BaseActivity {
         adapter = new MessageAdapter(mContext, R.layout.message_list_item, messageList);
         binding.messageListView.setAdapter(adapter);
 
+    }
+//    세칸을 다 채울때 까지.
+//    문제 배열엥 들어있나 검사. 안들어있으면 집어 넣자 => 들어있는걸 발견? 중복! 다시뽑자.
+//    컴퓨터가 1~9숫자 랜덤 생성
+//    컴퓨터 문제 출제.
+    void makeQuestion() {
+//        3자리 배열을 다 채울때까지.
+        for (int i = 0; i < questionNumArr.length; i++) {
+
+//        각 자리에 조건에 맞는 숫자를 뽑을때 까지.
+            while (true) {
+
+
+//                1~9사이의 랜덤값 뽑아 보자.
+//                1 <= (int) (Math.random()*9 + 1) < 10
+                int randomNum = (int) (Math.random() * 9 + 1);
+
+//                배열안에 랜덤값과 같은값이 들어있나?
+//                한번도 없으면 중복이 아니여서 사용해도 o
+//                한번이라도 찾으면 중복o,사용하면 x
+                boolean isNumOK = true;
+//                문제 배열안에있는 숫자를 num로 하나씩 뽑아보자
+                for (int num : questionNumArr) {
+                    if (randomNum == num) {
+                        isNumOK = false;
+                        break;
+                    }
+                }
+
+//                isNumOK가 끝까지 true로 남아있나?
+
+
+                if (isNumOK) {
+                    questionNumArr[i] = randomNum;
+                    Log.d("문제번호", randomNum + "");
+                    break;
+                }
+
+            }
+
+        }
+    }
+
+//    ?S?B 판정하기.
+
+    void checkStrikeAndBall(int inputNum) {
+
+//        123 => {1,2,3} 배열로 분리.
+        int[] userNumArr = new int[3];
+//        0번칸? 100의자리
+        userNumArr[0] = inputNum / 100;
+//        1번칸? 10의자리
+        userNumArr[1] = inputNum / 10 % 10;
+//        2번칸?1의 자리
+        userNumArr[2] = inputNum % 10;
+
+//        S가 몇개 , B이 몇개인지 카운팅.
+        int strikeCount = 0;
+        int ballCount = 0;
+
+//        숫자 비교 for 중첩
+        for (int i =0 ; i < userNumArr.length ; i++) {
+
+            for (int j =0; j < questionNumArr.length; j++) {
+
+                if (userNumArr[i] == questionNumArr[j]) {
+
+                    if (i == j) {
+                        strikeCount ++;
+                    }
+                    else {
+                        ballCount++;
+                    }
+
+
+                }
+
+            }
+
+        }
+
+        String content = String.format("%dS %dB 입니다.", strikeCount, ballCount);
+        messageList.add(new Message(content, "COMPUTER"));
+        adapter.notifyDataSetChanged();
+
+        binding.messageListView.smoothScrollToPosition(messageList.size()-1);
+
 
 
     }
+
+
 }
