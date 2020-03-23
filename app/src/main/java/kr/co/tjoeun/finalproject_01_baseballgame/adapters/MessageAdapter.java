cd2456b723aceae74344d85b12851314e3066656
saeyeong <syeongppp@gmail.com>
2020-03-23 19:51:54 +0900

    어댑터 / 리스트뷰 연결 컴퓨터 메세지 3개 추가 컴퓨터 / 사용자 메세지 구별 작업중

--- app/src/main/java/kr/co/tjoeun/finalproject_01_baseballgame/adapters/MessageAdapter.java
+++ app/src/main/java/kr/co/tjoeun/finalproject_01_baseballgame/adapters/MessageAdapter.java
@@ -5,6 +5,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.LinearLayout;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -37,7 +38,18 @@ public class MessageAdapter extends ArrayAdapter<Message> {
             row = inf.inflate(R.layout.message_list_item, null);
         }
 
-        return row;
+        Message data = mList.get(position);
 
+        LinearLayout computerMessageLayout = row.findViewById(R.id.computerMessageLayout);
+        LinearLayout userMessageLayout = row.findViewById(R.id.userMessageLayout);
+
+        if (data.getSpeaker().equals("COMPUTER")) {
+
+        }
+        else {
+
+        }
+
+        return row;
     }
 }
