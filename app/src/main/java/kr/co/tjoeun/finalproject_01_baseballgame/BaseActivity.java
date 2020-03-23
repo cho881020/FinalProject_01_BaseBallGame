af56532fe3624fdcdb09e63ca6dbd09bdda3fb41
alonso14ss <reiyan92@gmail.com>
2020-03-23 19:13:45 +0900

    주석 테스팅

--- app/src/main/java/kr/co/tjoeun/finalproject_01_baseballgame/BaseActivity.java
+++ app/src/main/java/kr/co/tjoeun/finalproject_01_baseballgame/BaseActivity.java
@@ -6,6 +6,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public abstract class BaseActivity extends AppCompatActivity {
 
+//    백승훈의 개발 브런치
+
     public Context mContext = this;
 
     public abstract void setupEvents();
