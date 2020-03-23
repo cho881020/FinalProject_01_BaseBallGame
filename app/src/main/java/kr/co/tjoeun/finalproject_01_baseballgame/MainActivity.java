a470207bc8662114e3d1ebe04306a1e6b3aaa6c0
cho881020 <kj_cho@nepp.kr>
2020-03-23 19:05:31 +0900

    Initial commit

---
+++ app/src/main/java/kr/co/tjoeun/finalproject_01_baseballgame/MainActivity.java
@@ -0,0 +1,33 @@
+package kr.co.tjoeun.finalproject_01_baseballgame;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
+
+import android.os.Bundle;
+
+import kr.co.tjoeun.finalproject_01_baseballgame.databinding.ActivityMainBinding;
+
+public class MainActivity extends BaseActivity {
+
+
+    ActivityMainBinding binding = null;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        setupEvents();
+        setValues();
+    }
+
+    @Override
+    public void setupEvents() {
+
+    }
+
+    @Override
+    public void setValues() {
+
+    }
+}
