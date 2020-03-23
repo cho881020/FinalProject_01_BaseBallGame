f396b89afeea26ec495d29620b2a5f072ec9191b
YUJIWAN <61855068+YUJIWAN@users.noreply.github.com>
2020-03-23 19:21:37 +0900

    데이터 클래스 생성

---
+++ app/src/main/java/kr/co/tjoeun/finalproject_01_baseballgame/data/Message.java
@@ -0,0 +1,30 @@
+package kr.co.tjoeun.finalproject_01_baseballgame.data;
+
+import java.io.Serializable;
+
+public class Message implements Serializable {
+
+    private String content;
+    private String speaker; //말한 사람. 내가? 컴퓨터?
+
+    public Message(String content, String speaker) {
+        this.content = content;
+        this.speaker = speaker;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getSpeaker() {
+        return speaker;
+    }
+
+    public void setSpeaker(String speaker) {
+        this.speaker = speaker;
+    }
+}
